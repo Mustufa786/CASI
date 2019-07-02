71092dc3289af6043428d10ca408c30496b8d148
ali.azaz <ali.azaz.alam@hotmail.com>
2019-07-02 11:32:42 +0500

    some changes

--- app/src/main/java/edu/aku/hassannaqvi/casi_2018/contracts/FormsContract.java
+++ app/src/main/java/edu/aku/hassannaqvi/casi_2018/contracts/FormsContract.java
@@ -14,7 +14,7 @@ import org.json.JSONObject;
 
 public class FormsContract {
 
-    private final String projectName = "National Nutrition Survey 2018";
+    private final String projectName = "Central Asia Stunting Initiative 2019";
     //private final String surveyType = "SN";
     private String _ID = "";
     private String _UID = "";
