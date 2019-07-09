295f09811c2fc31a0362bf6cb0ce6d55dae3b1a1
Mustufa786 <mustufa_ayub@hotmail.com>
2019-07-09 12:08:26 +0500

    some changes

--- app/src/main/java/edu/aku/hassannaqvi/casi_2019/ui/household/SectionA7Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/casi_2019/ui/household/SectionA7Activity.java
@@ -128,83 +128,83 @@ public class SectionA7Activity extends AppCompatActivity {
         secH7.put("cih70101", bi.cih701a.isChecked() ? "1"
                 : bi.cih701b.isChecked() ? "2" : "0");
         secH7.put("cih70102", bi.cih70102.getText().toString());
-        secH7.put("cih70103", bi.cih70103.getSelectedItem().toString());
+        secH7.put("cih70103", bi.cih70103.getText().toString());
 
 
         secH7.put("cih70201", bi.cih702a.isChecked() ? "1"
                 : bi.cih702b.isChecked() ? "2" : "0");
         secH7.put("cih70202", bi.cih70202.getText().toString());
-        secH7.put("cih70203", bi.cih70203.getSelectedItem().toString());
+        secH7.put("cih70203", bi.cih70203.getText().toString());
 
         secH7.put("cih70301", bi.cih703a.isChecked() ? "1"
                 : bi.cih703b.isChecked() ? "2" : "0");
         secH7.put("cih70302", bi.cih70302.getText().toString());
-        secH7.put("cih70303", bi.cih70303.getSelectedItem().toString());
+        secH7.put("cih70303", bi.cih70303.getText().toString());
 
 
         secH7.put("cih70401", bi.cih704a.isChecked() ? "1"
                 : bi.cih704b.isChecked() ? "2" : "0");
         secH7.put("cih70402", bi.cih70402.getText().toString());
-        secH7.put("cih70403", bi.cih70403.getSelectedItem().toString());
+        secH7.put("cih70403", bi.cih70403.getText().toString());
 
 
         secH7.put("cih70501", bi.cih705a.isChecked() ? "1"
                 : bi.cih705b.isChecked() ? "2" : "0");
         secH7.put("cih70502", bi.cih70502.getText().toString());
-        secH7.put("cih70503", bi.cih70503.getSelectedItem().toString());
+        secH7.put("cih70503", bi.cih70503.getText().toString());
 
         secH7.put("cih70601", bi.cih706a.isChecked() ? "1"
                 : bi.cih706b.isChecked() ? "2" : "0");
         secH7.put("cih70602", bi.cih70602.getText().toString());
-        secH7.put("cih70603", bi.cih70603.getSelectedItem().toString());
+        secH7.put("cih70603", bi.cih70603.getText().toString());
 
 
         secH7.put("cih70701", bi.cih707a.isChecked() ? "1"
                 : bi.cih707b.isChecked() ? "2" : "0");
         secH7.put("cih70702", bi.cih70702.getText().toString());
-        secH7.put("cih70703", bi.cih70703.getSelectedItem().toString());
+        secH7.put("cih70703", bi.cih70703.getText().toString());
 
 
         secH7.put("cih70801", bi.cih708a.isChecked() ? "1"
                 : bi.cih708b.isChecked() ? "2" : "0");
         secH7.put("cih70802", bi.cih70802.getText().toString());
-        secH7.put("cih70803", bi.cih70803.getSelectedItem().toString());
+        secH7.put("cih70803", bi.cih70803.getText().toString());
 
 
         secH7.put("cih70801", bi.cih708a.isChecked() ? "1"
                 : bi.cih708b.isChecked() ? "2" : "0");
         secH7.put("cih70802", bi.cih70802.getText().toString());
-        secH7.put("cih70803", bi.cih70803.getSelectedItem().toString());
+        secH7.put("cih70803", bi.cih70803.getText().toString());
 
 
         secH7.put("cih70901", bi.cih709a.isChecked() ? "1"
                 : bi.cih709b.isChecked() ? "2" : "0");
         secH7.put("cih70902", bi.cih70902.getText().toString());
-        secH7.put("cih70903", bi.cih70903.getSelectedItem().toString());
+        secH7.put("cih70903", bi.cih70903.getText().toString());
 
 
         secH7.put("cih71001", bi.cih710a.isChecked() ? "1"
                 : bi.cih710b.isChecked() ? "2" : "0");
         secH7.put("cih71002", bi.cih71002.getText().toString());
-        secH7.put("cih71003", bi.cih71003.getSelectedItem().toString());
+        secH7.put("cih71003", bi.cih71003.getText().toString());
 
 
         secH7.put("cih71101", bi.cih711a.isChecked() ? "1"
                 : bi.cih711b.isChecked() ? "2" : "0");
         secH7.put("cih71102", bi.cih71102.getText().toString());
-        secH7.put("cih71103", bi.cih71103.getSelectedItem().toString());
+        secH7.put("cih71103", bi.cih71103.getText().toString());
 
 
         secH7.put("cih71201", bi.cih712a.isChecked() ? "1"
                 : bi.cih712b.isChecked() ? "2" : "0");
         secH7.put("cih71202", bi.cih71202.getText().toString());
-        secH7.put("cih71203", bi.cih71203.getSelectedItem().toString());
+        secH7.put("cih71203", bi.cih71203.getText().toString());
 
 
         secH7.put("cih71301", bi.cih713a.isChecked() ? "1"
                 : bi.cih713b.isChecked() ? "2" : "0");
         secH7.put("cih71302", bi.cih71302.getText().toString());
-        secH7.put("cih71303", bi.cih71303.getSelectedItem().toString());
+        secH7.put("cih71303", bi.cih71303.getText().toString());
 
         secH7.put("cih714", bi.cih714.getText().toString());
         secH7.put("cih715", bi.cih715.getText().toString());
