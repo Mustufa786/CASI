cf515965e62ecb0c69899ff3c831aa4f1fb08e11
hassan.naqvi <shznaqvi@gmail.com>
2019-09-20 21:27:10 +0500

    Komil Corrections #1

--- app/src/main/java/edu/aku/hassannaqvi/casi_2019/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/casi_2019/core/MainApp.java
@@ -84,12 +84,12 @@ public class MainApp extends Application {
     public static final long MILLISECONDS_IN_2Years = MILLIS_IN_SECOND * SECONDS_IN_MINUTE * MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_2_YEAR;
     //    public static String _IP2 = "58.65.211.13"; // Test PHP server
     //    public static String _IP = "43.245.131.159"; // Test PHP server
-    public static String _IP = "f38158"; // Test PHP server
-    // public static String _IP = "vcoe1.aku.edu"; // Live PHP server
+    //public static String _IP = "f38158"; // Test PHP server
+    public static String _IP = "vcoe1.aku.edu"; // Live PHP server
 
     public static boolean isAttitudeCheck = false;
-    public static String _HOST_URL = "http://" + MainApp._IP + "/casi/api/";
-    public static String _UPDATE_URL = "http://" + MainApp._IP + "/casi/app/";
+    public static String _HOST_URL = "https://" + MainApp._IP + "/casi/api/";
+    public static String _UPDATE_URL = "https://" + MainApp._IP + "/casi/app/";
     public static String deviceId;
 
     public static Boolean admin = false;
@@ -538,10 +538,10 @@ public class MainApp extends Application {
         //  TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/JameelNooriNastaleeq.ttf");
 
         // font from assets: font for Farsi
-        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/uthman_dari.ttf");
+        //TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/uthman_dari.ttf");
 
         //font for Tajik
-//        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/Arimo-Regular.ttf");
+        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/Arimo-Regular.ttf");
 
 
         deviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(),
